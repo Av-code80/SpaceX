5604dbb28dceac892aaf296cff5eb53cbaa8473e
avehghasemian <av.ghasemian@gmail.com>
2021-09-19 01:43:52 +0200

    ButtonAnimation

---
+++ script.js
@@ -0,0 +1,9 @@
+const card = $(".perspective__3d");
+
+$("body").on("mousemove", function (e) {
+  let vertical = -($(window).innerWidth() / 2 - e.pageX) / 70,
+    horizontal = ($(window).innerHeight() / 2 - e.pageY) / 70;
+
+  card.attr( "style", "transform: rotateY(" + vertical + "deg) rotateX(" + horizontal + "deg)"
+  );
+});
